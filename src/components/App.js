407cf424d105a9028e2e02566df58c0db74b9bcf
Chandrashekar185 <chanduminny32@gmail.com>
2025-07-24 19:43:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import React from "./components/ReactComp";
+
 
 const App = () => {
   return (
     <div id="main"></div>
+  <React>
   )
 }
 
